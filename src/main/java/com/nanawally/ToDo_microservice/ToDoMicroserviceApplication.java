e82742113084951f81b6db61eabd691c32f59a31
Anna Wallström <anoli89@hotmail.com>
2025-11-17 14:47:14 +0100

    Initial commit

---
+++ src/main/java/com/nanawally/ToDo_microservice/ToDoMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.nanawally.ToDo_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nanawally/ToDo_microservice/ToDoMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanawally.ToDo_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
