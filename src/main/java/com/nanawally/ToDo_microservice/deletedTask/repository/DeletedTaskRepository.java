fd9adeed61a595dc1323790224f99b7428734a18
liluppis <lisa.upp@hotmail.com>
2025-11-19 11:25:07 +0100

    add: query method for findByTag() in DeletedTaskRepository & implementation in DeletedTaskService

--- src/main/java/com/nanawally/ToDo_microservice/deletedTask/repository/DeletedTaskRepository.java
+++ src/main/java/com/nanawally/ToDo_microservice/deletedTask/repository/DeletedTaskRepository.java
@@ -1,11 +1,18 @@
 package com.nanawally.ToDo_microservice.deletedTask.repository;
 
 import com.nanawally.ToDo_microservice.deletedTask.model.DeletedTask;
+import com.nanawally.ToDo_microservice.task.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface DeletedTaskRepository extends JpaRepository<DeletedTask, UUID> {
+
+    @Query("SELECT d FROM DeletedTask d JOIN d.tags tag WHERE tag = :tag")
+    List<DeletedTask> findByTag(@Param("tag") String tag);
 }
---
+++ src/main/java/com/nanawally/ToDo_microservice/service/DeletedTaskService.java
@@ -0,0 +1,58 @@
+package com.nanawally.ToDo_microservice.service;
+
+import com.nanawally.ToDo_microservice.deletedTask.mapper.DeletedTaskMapper;
+import com.nanawally.ToDo_microservice.deletedTask.model.DeletedTask;
+import com.nanawally.ToDo_microservice.deletedTask.model.dto.DeletedTaskDTO;
+import com.nanawally.ToDo_microservice.deletedTask.repository.DeletedTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class DeletedTaskService {
+
+    private final DeletedTaskRepository deletedTaskRepository;
+    private final DeletedTaskMapper deletedTaskMapper;
+
+    @Autowired
+    public DeletedTaskService(DeletedTaskRepository deletedTaskRepository, DeletedTaskMapper deletedTaskMapper) {
+        this.deletedTaskRepository = deletedTaskRepository;
+        this.deletedTaskMapper = deletedTaskMapper;
+    }
+
+    // GET - all
+    public List<DeletedTaskDTO> findAllDeletedTasks(){
+        return deletedTaskRepository.findAll().stream().map(deletedTaskMapper::mapToDeletedTaskDTO).collect(Collectors.toList());
+    }
+
+    // GET - filtered
+    public List<DeletedTaskDTO> findDeletedTaskByTag(String tag){
+        return deletedTaskRepository.findByTag(tag).stream().map(deletedTaskMapper::mapToDeletedTaskDTO).collect(Collectors.toList());
+    }
+
+
+    // DELETE - by id
+    public boolean deleteTaskFromTrash(UUID id) {
+        DeletedTask taskToDelete = deletedTaskRepository.findById(id).orElse(null);
+
+        if (taskToDelete != null) {
+            deletedTaskRepository.delete(taskToDelete);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // DELETE - all
+    public boolean deleteAllTasks() {
+        if (deletedTaskRepository.findAll().isEmpty()) {
+            return false;
+        } else {
+            deletedTaskRepository.deleteAll();
+            return true;
+        }
+    }
+}
