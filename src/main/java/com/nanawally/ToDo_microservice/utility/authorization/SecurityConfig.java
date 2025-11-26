98c397bd49f0e162c22570653eb6af2d2f35f68a
Anna Wallström <anoli89@hotmail.com>
2025-11-26 16:06:04 +0100

    Add cors config

--- src/main/java/com/nanawally/ToDo_microservice/utility/authorization/SecurityConfig.java
+++ src/main/java/com/nanawally/ToDo_microservice/utility/authorization/SecurityConfig.java
@@ -4,12 +4,17 @@ import com.nanawally.ToDo_microservice.utility.authorization.jwt.JwtAuthenticati
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -22,16 +27,32 @@ public class SecurityConfig {
     public SecurityFilterChain todoSecurityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(
-                        SessionCreationPolicy.STATELESS
-                ))
+                                SessionCreationPolicy.STATELESS
+                        ))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/v2/tasks/**").authenticated()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/v2/tasks").permitAll()
+                        .requestMatchers("/v2/tasks/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 
 }
